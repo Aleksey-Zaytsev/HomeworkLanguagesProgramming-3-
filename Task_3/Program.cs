3d4467fe4e26ed4ddc0cdb1351135f97d1f0af5d
Aleksey <a.y.zayitsev@gmail.com>
2023-01-10 00:21:51 +0300

    Программа котрая выдет таблицу кубов введенного числа

---
+++ Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = num + 1;
+Console.WriteLine ($"Таблица кубов чисел от 1 до {num} : ");
+for (int i = 1; i < num2; i++)
+{
+    Console.WriteLine (i*i*i);
+}
